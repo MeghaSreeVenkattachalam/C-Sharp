55f0af8a1c0f488cfe464d32ed4c78db7b3c36e3
MEGHA SREE VENKATTACHALAM <meghavenkattachalam@gmail.com>
2025-08-26 22:47:33 +0530

    Create 9. Palindrome Number.cs

---
+++ LeetCode/9. Palindrome Number.cs
@@ -0,0 +1,46 @@
+Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+ 
+
+Constraints:
+
+-231 <= x <= 231 - 1
+
+CODE:
+
+public class Solution {
+    public bool IsPalindrome(int x) {
+        int res = 0;
+        int val = x;
+        while(val>0){
+            res = res*10+val%10;
+            val /= 10;
+        }
+        return res==x;
+    }
+}
+
+OUTPUT:
+
+Runtime
+1
+ms
+Beats
+99.96%
